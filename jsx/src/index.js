5ffc9fc9878dfe5e170a6b017863b35c9e4efb6f
Thomk <thomkok13@hotmail.com>
2020-09-11 14:59:44 +0200

    const values added to use in the App component.

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,8 +4,17 @@ import ReactDOM from 'react-dom';
 
 // Create a react component.
 const App = () => {
-    return <div>Hi there!</div>
-}
+    const buttonText = {text: "click me"};
+    const labelText = "Enter name:";
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">{labelText}</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: "blue", color: "white"}}>{buttonText.text}</button>
+        </div>
+    );
+};
 
 // Take the react component and show it on the screen.
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'));
