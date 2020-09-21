14b00eba64d347a28a1f4384998cb5a1da01a4b3
thomk <thomkok13@hotmail.com>
2020-09-21 11:37:51 +0200

    unsplash api added.

---
+++ pics/src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID 2vmmljrnkHO4LCRgj30_UWiHGuUuHoCIauuPoXNMSJQ'
+    }
+});
