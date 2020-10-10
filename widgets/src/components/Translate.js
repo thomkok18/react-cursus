73dbc256cae97887b8c8469438c9fc84b1fc0c23
Thom Kok <thomkok13@hotmail.com>
2020-10-10 14:28:16 +0200

    Dutch language added. Input field created to enter text. Convert component added to show translated text.

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 
 const options = [
     {
@@ -13,15 +14,34 @@ const options = [
     {
         label: 'Hindi',
         value: 'hi'
+    },
+    {
+        label: 'Dutch',
+        value: 'nl'
     }
 ];
 
 const Translate = () => {
     const [language, setLanguage] = useState(options[0]);
+    const [text, setText] = useState('')
 
     return (
         <div>
-            <Dropdown label="Select a Language" selected={language} onSelectedChange={setLanguage} options={options} />
+            <div className='ui form'>
+                <div className='field'>
+                    <label>Enter Text</label>
+                    <input value={text} onChange={(e) => setText(e.target.value)}/>
+                </div>
+            </div>
+            <Dropdown
+                label="Select a Language"
+                selected={language}
+                onSelectedChange={setLanguage}
+                options={options}
+            />
+            <hr />
+            <h3 className='ui header'>Output</h3>
+            <Convert text={text} language={language} />
         </div>
     );
 }
