a6b445e927ef299a6d71644e88df78f153a2dcef
thomk <thomkok13@hotmail.com>
2020-10-12 13:12:43 +0200

    reminder

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import key from "./key";
 
-const KEY = 'AIzaSyBGHqx-nBgbkmqL7wU6Zj-jrKQZazmTLKY';
+// Go to https://console.developers.google.com/apis/dashboard.
+const KEY = key;
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
