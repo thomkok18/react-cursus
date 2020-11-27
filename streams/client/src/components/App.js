799105dfd0180189723c2c23315e89e3c94b99fd
thomkok18 <thomkok13@hotmail.com>
2020-11-27 15:11:42 +0100

    Streaming urls added. Reducers added. Routing added. Custom history file added. and more...

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {Router, Route} from 'react-router-dom';
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import history from "../history";
 
 const App = () => {
     return (
-        <div>App</div>
+        <div className='ui container'>
+            <Router history={history}>
+                <Header />
+                <Route path='/' exact component={StreamList} />
+                <Route path='/streams/new' exact component={StreamCreate} />
+                <Route path='/streams/edit/:id' exact component={StreamEdit} />
+                <Route path='/streams/delete/:id' exact component={StreamDelete} />
+                <Route path='/streams/show/:id' exact component={StreamShow} />
+            </Router>
+        </div>
     );
 };
 
